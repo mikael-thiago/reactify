7f37189eb516fd6b8065b59b2faaabc90683243b
Mikael <mikael.thiago@hotmail.com>
2020-07-20 02:32:39 -0300

    retirada a responsabilidade da sidebar de renderizar seus sidebarlinks com a classe active. sidebarlinks agora se autogerenciam em relação a classe active. logout ajustado

--- front-end/src/principal/components/Sidebar/Sidebar.jsx
+++ front-end/src/principal/components/Sidebar/Sidebar.jsx
@@ -17,17 +17,15 @@ const PlaylistItem = ({ name = "" }) => {
     )
 }
 
-const Sidebar = ({ match, history }) => {
+const Sidebar = ({ match, history, setLoggedIn }) => {
 
     const authorizationData = getToken();
 
     const [playlists, setPlaylists] = useState([]);
 
-    const dispatch = useDispatch();
-
     const logout = () => {
         tokenLogout();
-        history.push("/");
+        setLoggedIn(false);
     }
 
     useEffect(() => {
@@ -47,16 +45,6 @@ const Sidebar = ({ match, history }) => {
             })
         }
 
-
-        const urlRoute = window.location.href.split("/")[window.location.href.split("/").length - 1];
-
-        if (urlRoute === "search")
-            dispatch(setActiveItem({ activeItem: "Buscar" }));
-        else if (urlRoute === "#2")
-            dispatch(setActiveItem({ activeItem: "Sua Biblioteca" }));
-        else
-            dispatch(setActiveItem({ activeItem: "Início" }));
-
     }, []);
 
     return (
@@ -66,9 +54,9 @@ const Sidebar = ({ match, history }) => {
             </div>
             <div className="sidebar-body">
                 <div className="sidebar-body-navs">
-                    <SidebarLink name="Início" to={`${match.url}`} xmlns="http://www.w3.org/2000/svg" d="M448 463.746h-149.333v-149.333h-85.334v149.333h-149.333v-315.428l192-111.746 192 110.984v316.19z" />
-                    <SidebarLink name="Buscar" to={`${match.url}/search`} xmlns="http://www.w3.org/2000/svg" d="M357.079 341.334l94.476 110.73-32.508 27.683-94.222-110.476q-45.714 30.476-100.826 30.476-36.826 0-70.476-14.349t-57.905-38.603-38.603-57.905-14.349-70.476 14.349-70.476 38.603-57.905 57.905-38.603 70.476-14.349 70.476 14.349 57.905 38.603 38.603 57.905 14.349 70.476q0 23.365-5.841 45.714t-16.635 41.651-25.778 35.555zM224 357.079q28.19 0 53.841-11.048t44.19-29.587 29.587-44.19 11.048-53.841-11.048-53.841-29.587-44.191-44.19-29.587-53.841-11.047-53.841 11.047-44.191 29.588-29.587 44.19-11.047 53.841 11.047 53.841 29.588 44.19 44.19 29.587 53.841 11.048z" />
-                    <SidebarLink name="Sua Biblioteca" to={`${match.url}/#2`} xmlns="http://www.w3.org/2000/svg" d="M311.873 77.46l166.349 373.587-39.111 17.27-166.349-373.587zM64 463.746v-384h42.666v384h-42.666zM170.667 463.746v-384h42.667v384h-42.666z" />
+                    <SidebarLink name="Início" to={""} xmlns="http://www.w3.org/2000/svg" d="M448 463.746h-149.333v-149.333h-85.334v149.333h-149.333v-315.428l192-111.746 192 110.984v316.19z" />
+                    <SidebarLink name="Buscar" to={"/search"} xmlns="http://www.w3.org/2000/svg" d="M357.079 341.334l94.476 110.73-32.508 27.683-94.222-110.476q-45.714 30.476-100.826 30.476-36.826 0-70.476-14.349t-57.905-38.603-38.603-57.905-14.349-70.476 14.349-70.476 38.603-57.905 57.905-38.603 70.476-14.349 70.476 14.349 57.905 38.603 38.603 57.905 14.349 70.476q0 23.365-5.841 45.714t-16.635 41.651-25.778 35.555zM224 357.079q28.19 0 53.841-11.048t44.19-29.587 29.587-44.19 11.048-53.841-11.048-53.841-29.587-44.191-44.19-29.587-53.841-11.047-53.841 11.047-44.191 29.588-29.587 44.19-11.047 53.841 11.047 53.841 29.588 44.19 44.19 29.587 53.841 11.048z" />
+                    <SidebarLink name="Sua Biblioteca" to={"/#2"} xmlns="http://www.w3.org/2000/svg" d="M311.873 77.46l166.349 373.587-39.111 17.27-166.349-373.587zM64 463.746v-384h42.666v384h-42.666zM170.667 463.746v-384h42.667v384h-42.666z" />
                 </div>
                 <div className="sidebar-body-playlists">
                     <div className="sidebar-body-playlists-title">
--- front-end/src/principal/components/Sidebar/SidebarLink.jsx
+++ front-end/src/principal/components/Sidebar/SidebarLink.jsx
@@ -1,28 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { setRoute } from "../../../redux/slices/routeSlice.js";
-import { setActiveItem } from "../../../redux/slices/sidebarSlice.js";
+import { useSelector } from "react-redux";
 
 const SidebarLink = ({ name, to = "", xmlns = "", d = "" }) => {
 
-    const sidebarState = useSelector((state) => state.sidebar);
+    const routeState = useSelector((state) => state.route);
 
-    const activeClass = (sidebarState.activeItem === name) ? " active" : "";
-
-    const dispatch = useDispatch();
-
-    const handleClick = () => {
-        dispatch(setRoute({ route: name }));
-        dispatch(setActiveItem({ activeItem: name }));
-    }
-
-    if (activeClass === " active") {
-        dispatch(setRoute({ route: name }));
-    }
+    const activeClass = (routeState.route === name) ? " active" : "";
 
     return (
-        <div className={"sidebar-item" + activeClass} onClick={handleClick}>
+        <div className={"sidebar-item" + activeClass}>
             <Link to={to}>
                 <div className="sidebar-item-icon">
                     <svg viewBox="0 0 512 512" width="24" height="24" xmlns={xmlns}><path d={d} fill="currentColor">
