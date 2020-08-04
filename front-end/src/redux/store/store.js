41a0f3ddef4493e6d87373a9eba328367951f9a5
Mikael <mikael.thiago@hotmail.com>
2020-08-04 03:45:52 -0300

    removido redux de onde era desnecessário

--- front-end/src/redux/store/store.js
+++ front-end/src/redux/store/store.js
@@ -1,15 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import playerReducer from "../slices/playerSlice.js";
-import searchReducer from "../slices/searchSlice.js";
-import routeReducer from "../slices/routeSlice.js";
-import sidebarReducer from "../slices/sidebarSlice.js";
 
 export default configureStore({
     reducer: {
         player: playerReducer,
-        search: searchReducer,
-        route: routeReducer,
-        sidebar: sidebarReducer
     }
 })
