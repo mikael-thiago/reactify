26a5b1491d8803f642e015ad9c81ef5adfa27365
Mikael <mikael.thiago@hotmail.com>
2020-08-07 13:52:32 -0300

    função de converter tempo movida para pasta utils

--- front-end/src/pages/principal/components/TrackRow/TrackRow.jsx
+++ front-end/src/pages/principal/components/TrackRow/TrackRow.jsx
@@ -1,50 +1,22 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setTrack } from "../../../../redux/slices/playerSlice.js";
 
 import music from "../../../../images/musica.svg";
 import play from "../../../../images/play.svg";
 
 import "./trackRow.css";
+import { parseDurationTime } from "../../../../utils/time.js";
 
-const parseDurationTime = (ms) => {
 
-    const HOUR_IN_MS = 3600000;
-    const MINUTES_IN_MS = 60000;
-    const SECONDS_IN_MS = 1000;
-
-    let hours = 0, minutes = 0, seconds = 0;
-
-    if (ms > HOUR_IN_MS) {
-        hours = (ms / HOUR_IN_MS);
-        ms = ms % HOUR_IN_MS;
-    }
-
-    if (ms > MINUTES_IN_MS) {
-        minutes += ms / MINUTES_IN_MS;
-        ms = ms % MINUTES_IN_MS;
-    }
-
-    if (ms > SECONDS_IN_MS) {
-        seconds += ms / SECONDS_IN_MS;
-        ms = ms % SECONDS_IN_MS;
-    }
-
-    hours = parseInt(hours);
-    minutes = parseInt(minutes);
-    seconds = parseInt(seconds);
-
-    return { hours: hours, minutes: minutes, seconds: seconds };
-
-}
-
-const TrackRow = ({ track = {}, albumPhotoUrl = "", showArtists = false, showImage = false }) => {
+const TrackRow = ({ track = {}, showArtists = false, showImage = false }) => {
 
     const [hover, setHover] = useState(false);
 
     const dispatch = useDispatch();
 
     const musicDurationTime = () => {
+
         const duration_ms = track.duration_ms;
 
         let { hours, minutes, seconds } = parseDurationTime(duration_ms);
@@ -65,7 +37,7 @@ const TrackRow = ({ track = {}, albumPhotoUrl = "", showArtists = false, showIma
     return (
         <div className="track-row" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <div className="music-icon" onClick={playMusic}>
-                <img src={hover ? play : music}></img>
+                <img src={hover ? play : music} alt=""></img>
             </div>
 
             {showImage && track.album.images[0] ?
