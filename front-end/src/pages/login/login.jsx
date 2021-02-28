f2a873b58e3cf1b2092b742221caa98f948c5f71
Mikael <mikaelbad12@academico.ufs.br>
2021-02-28 00:15:27 -0300

    página de login modificada

--- front-end/src/pages/login/login.jsx
+++ front-end/src/pages/login/login.jsx
@@ -1,64 +1,53 @@
-import React, { useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
 
-import { login } from '../../services/token_manipulation';
+import { login } from "../../services/token_manipulation";
 
 //Utils
-import { getQueryParams } from '../../utils/url';
+import { getQueryParams } from "../../utils/url";
 
 //Styles
 import "./login.css";
 
 const LoginCard = withRouter(({ history }) => {
+  useEffect(() => {
+    let params = getQueryParams(window.location.href);
 
-    useEffect(() => {
+    let access_token = params.access_token;
+    let refresh_token = params.refresh_token;
 
-        let params = getQueryParams(window.location.href);
+    if (access_token !== null && access_token !== undefined) {
+      login(access_token, refresh_token);
 
-        let access_token = params.access_token;
-        let refresh_token = params.refresh_token;
+      history.push("/on");
+    }
+  }, []);
 
-        if (access_token !== null && access_token !== undefined) {
+  return (
+    <div className="login-card">
+      <div className="login-card-header"></div>
 
-            login(access_token, refresh_token);
-
-            history.push("/on");
-
-        }
-
-    }, []);
-
-    return (
-        <div className="login-card">
-            <div className="login-card-header">
-
-            </div>
-
-            <div className="login-card-body">
-                <form className="login-form">
-
-                    <div style={{ width: "100%", justifyContent: "center", display: "flex", flexDirection: "row" }}>
-                        <button><a href="http://localhost:4000/authenticate/">Login</a></button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    );
+      <div className="login-card-body">
+        <form className="login-form">
+          <div style={{ width: "100%", justifyContent: "center", display: "flex", flexDirection: "row" }}>
+            <button>
+              <a href="http://localhost:4000/authenticate/">Login</a>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 });
 
 const LoginPage = (props) => {
+  return (
+    <div className="login-wrapper">
+      <LoginCard />
 
-    return (
-        <div className="login-wrapper">
-
-            <LoginCard />
-
-            <div className="login-body">
-
-            </div>
-        </div>
-    );
-}
+      <div className="login-body"></div>
+    </div>
+  );
+};
 
 export default LoginPage;
